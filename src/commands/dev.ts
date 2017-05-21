833d75bd14a2668182c9ca2898942a3f255059f1
Aaron Tribou <aaron.tribou@gmail.com>
2017-05-21 15:59:15 -0500

    Add initial prepublish script

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -1,11 +1,11 @@
 import Debug from 'debug'
-import Nodemon from 'nodemon'
-import { IYargs } from './'
+// Import Nodemon from 'nodemon'
+import { IYargs } from '../'
 import { yarn } from './yarn'
 
 const log: Function = Debug('fob:dev')
 
-export async function dev (yargs: IYargs): void {
+export async function dev (yargs: IYargs): Promise<void> {
 
   log('Starting dev', yargs)
   await yarn()
